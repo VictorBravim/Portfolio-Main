b9c30bd10938af14ebed73832a66fda2f0656559
Victor Bravim <victorrafaelbravim@gmail.com>
2024-10-11 15:59:18 -0300

    Add

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -34,15 +34,16 @@ const Hero: React.FC = () => {
                     className="bg-[#3d3d3d36] backdrop-blur-[2px] border border-white border-opacity-10 rounded-full flex p-1 space-x-4 mb-2"
                     variants={itemVariants}
                 >
-                    <button className="bg-[#3d3d3d36] backdrop-blur-[2px] border border-white border-opacity-10 px-2 lg:px-6 text-[18px] lg:text-[24px] rounded-full">
-                        Desenvolvedor Front-End.
+                    <button className="bg-[#3d3d3d36] backdrop-blur-[2px] border border-white border-opacity-10 px-4 lg:px-6 text-[16px] lg:text-[24px] rounded-full">
+                        <span className="block lg:hidden">Front-End</span>
+                        <span className="hidden lg:block">Desenvolvedor Front-End</span>
                     </button>
-                    <button className="px-2 lg:px-6 text-[18px] lg:text-[24px] bg-none rounded-full">
+                    <button className="px-2 lg:px-6 text-[16px] lg:text-[24px] bg-none rounded-full">
                         UI Designer
                     </button>
                 </motion.div>
 
-                <motion.h1 className="font-inter text-[50px] lg:text-[85px] font-bold text-center leading-tight" variants={itemVariants}>
+                <motion.h1 className="font-inter text-[50px] lg:text-[85px] font-bold text-center leading-tight w-full" variants={itemVariants}>
                     <span className='text-[#0086B0]'>Transformando conceitos</span> em <br /> experiências de usuários
                 </motion.h1>
 
@@ -51,30 +52,30 @@ const Hero: React.FC = () => {
                 </motion.p>
 
                 <motion.div className="mt-8 space-x-4 flex flex-row justify-center" variants={itemVariants}>
-    <button 
-        className="text-[18px] lg:text-[24px] font-bold px-8 py-1.5 bg-gradient-to-b from-[#8c8c8c25] to-[#3d3d3d36] backdrop-blur-[4px] border border-white border-opacity-10 rounded-full"
-        onClick={() => {
-            document.getElementById('footer')?.scrollIntoView({ behavior: 'smooth' });
-        }}
-    >
-        Contato
-    </button>
-    <a
-        href="https://wa.me/5512996839077" 
-        target="_blank" 
-        rel="noopener noreferrer"
-    >
-        <button 
-            className="text-[18px] lg:text-[24px] font-bold px-8 py-1.5 bg-gradient-to-b from-[#8c8c8c25] to-[#3d3d3d36] backdrop-blur-[4px] border border-white border-opacity-10 rounded-full"
-        >
-            Orçamento
-        </button>
-    </a>
-</motion.div>
+                    <button
+                        className="text-[18px] lg:text-[24px] font-bold px-8 py-1.5 bg-gradient-to-b from-[#8c8c8c25] to-[#3d3d3d36] backdrop-blur-[4px] border border-white border-opacity-10 rounded-full"
+                        onClick={() => {
+                            document.getElementById('footer')?.scrollIntoView({ behavior: 'smooth' });
+                        }}
+                    >
+                        Contato
+                    </button>
+                    <a
+                        href="https://wa.me/5512996839077"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <button
+                            className="text-[18px] lg:text-[24px] font-bold px-8 py-1.5 bg-gradient-to-b from-[#8c8c8c25] to-[#3d3d3d36] backdrop-blur-[4px] border border-white border-opacity-10 rounded-full"
+                        >
+                            Orçamento
+                        </button>
+                    </a>
+                </motion.div>
 
 
                 {/* Seção de projetos */}
-                <motion.div className="grid gap-4 mt-8 lg:mt-32 w-full px-4 sm:px-20 md:px-40" variants={containerVariants}>
+                <motion.div className="grid gap-4 mt-8 lg:mt-32 w-full px-4 sm:px-20 lg:px-40" variants={containerVariants}>
                     <motion.div className="flex flex-col md:flex-row justify-center gap-8 mb-2" variants={itemVariants}>
                         <div className="bg-gradient-to-b from-[#8c8c8c10] to-[#3d3d3d36] backdrop-blur-[4px] border border-white border-opacity-10 w-full md:w-[65%] h-[40vw] md:h-[25vw] rounded-2xl flex items-center justify-center p-2">
                             <div className="bg-gradient-to-b from-[#8c8c8c0c] to-[#3d3d3d36] backdrop-blur-[4px] border border-white border-opacity-10 w-full h-full rounded-2xl flex items-center justify-center p-4">
